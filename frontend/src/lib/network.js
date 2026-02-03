4c02bf12f02bacc08859014142d5bbb1947ba1e2
Andre Rivera <andre.rivera108@gmail.com>
2026-02-02 23:49:22 -0500

    Add network configuration for zkSYS devnet

---
+++ frontend/src/lib/network.js
@@ -0,0 +1,10 @@
+export const ZKSYS_DEVNET = {
+  chainId: 57042,
+  chainName: 'zkSYS PoB Devnet',
+  rpcUrls: ['https://rpc-pob.dev11.top'],
+  nativeCurrency: {
+    name: 'TSYS',
+    symbol: 'TSYS',
+    decimals: 18
+  }
+};
