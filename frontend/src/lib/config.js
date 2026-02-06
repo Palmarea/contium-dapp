992b96f753ae1d60ae1724553ebefd523dabbe9e
palmarea <masp6281@gmail.com>
2026-02-05 19:31:40 -0500

    feat(SC-06) adding addresses and ABIs de contracts deployed

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -3,22 +3,11 @@
  * zkSYS PoB DevNet
  */
 export const NETWORK = {
-  /** Chain ID decimal */
   chainId: 57042,
-
-  /** Chain ID hexadecimal (MetaMask) */
   chainIdHex: '0xDEB2',
-
-  /** Nombre de la red */
   name: 'zkSYS PoB DevNet',
-
-  /** RPC público */
   rpcUrl: 'https://rpc-pob.dev11.top',
-
-  /** Explorador de bloques */
   explorer: 'https://explorer-pob.dev11.top',
-
-  /** Moneda nativa */
   currency: {
     name: 'TSYS',
     symbol: 'TSYS',
@@ -30,77 +19,73 @@ export const NETWORK = {
  * Direcciones de contratos desplegados
  */
 export const CONTRACTS = {
-  /** Registro de documentos (hashes, metadata) */
-  documentRegistry: '0x0000...',
-
-  /** NFT / Badge de validación Contium */
-  contiumBadge: '0x0000...'
+  documentRegistry: '0xd707cc8D9FC170fe100147a8903e3DB33D596322',
+  contiumBadge: '0x912675023673C6BD0045630194caeA746B564959'
 };
 
 /**
  * ABIs de contratos inteligentes
- * (importar aquí cuando estén definidos)
  */
 export const ABIS = {
-  /** ABI del contrato DocumentRegistry */
-  documentRegistry: [],
-
-  /** ABI del contrato ContiumBadge (NFT) */
-  contiumBadge: []
+  documentRegistry: [
+    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"hash","type":"bytes32"},{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"}],"name":"DocumentRegistered","type":"event"},
+    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"bytes32","name":"hash","type":"bytes32"},{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"newScore","type":"uint256"}],"name":"DocumentValidated","type":"event"},
+    {"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"getDocument","outputs":[{"components":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"bool","name":"isValidated","type":"bool"},{"internalType":"string","name":"metadata","type":"string"}],"internalType":"struct DocumentRegistry.DocumentInfo","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"address","name":"_user","type":"address"}],"name":"getScore","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"uint256","name":"_limit","type":"uint256"}],"name":"getTopUsers","outputs":[{"internalType":"address[]","name":"topUsers","type":"address[]"},{"internalType":"uint256[]","name":"topScores","type":"uint256[]"}],"stateMutability":"view","type":"function"},
+    {"inputs":[],"name":"getTotalUsers","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"isRegistered","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"isValidated","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"},{"internalType":"string","name":"_metadata","type":"string"}],"name":"registerDocument","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[{"internalType":"bytes32","name":"_hash","type":"bytes32"}],"name":"validateDocument","outputs":[],"stateMutability":"nonpayable","type":"function"}
+  ],
+  contiumBadge: [
+    {"inputs":[{"internalType":"address","name":"_documentRegistry","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},
+    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},
+    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},
+    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},
+    {"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},
+    {"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},
+    {"inputs":[],"name":"documentRegistry","outputs":[{"internalType":"contract IDocumentRegistry","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"bytes32","name":"documentHash","type":"bytes32"}],"name":"mintBadge","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+    {"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},
+    {"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},
+    {"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},
+    {"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}
+  ]
 };
 
 /* -------------------------------------------------------------------------- */
 /*                                   Helpers                                  */
 /* -------------------------------------------------------------------------- */
 
-/**
- * Devuelve la URL del explorador para una transacción
- * @param {string} txHash - Hash de la transacción
- * @returns {string}
- */
 export function getTxExplorerUrl(txHash) {
   return `${NETWORK.explorer}/tx/${txHash}`;
 }
 
-/**
- * Devuelve la URL del explorador para una dirección
- * @param {string} address - Dirección wallet o contrato
- * @returns {string}
- */
 export function getAddressExplorerUrl(address) {
   return `${NETWORK.explorer}/address/${address}`;
 }
 
-/**
- * Devuelve la URL del explorador para un NFT
- * @param {string} contract - Dirección del contrato NFT
- * @param {string|number} tokenId - ID del token
- * @returns {string}
- */
 export function getNftExplorerUrl(contract, tokenId) {
   return `${NETWORK.explorer}/token/${contract}?a=${tokenId}`;
 }
 
-/**
- * Trunca una dirección para mostrarla en UI
- * Ej: 0x1234...ABCD
- *
- * @param {string} address - Dirección completa
- * @param {number} [start=6] - Caracteres iniciales
- * @param {number} [end=4] - Caracteres finales
- * @returns {string}
- */
 export function truncateAddress(address, start = 6, end = 4) {
   if (!address) return '';
   return `${address.slice(0, start)}...${address.slice(-end)}`;
 }
 
-/**
- * Parámetros para agregar la red en MetaMask
- * usando wallet_addEthereumChain
- *
- * @returns {import('ethers').providers.Networkish}
- */
 export function getMetaMaskNetworkParams() {
   return {
     chainId: NETWORK.chainIdHex,
